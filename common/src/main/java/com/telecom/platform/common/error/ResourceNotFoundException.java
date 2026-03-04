e082f0ae71e1b7237ccc190d365aa85cd282b8a0
shotaroi <58347058+shotaroi@users.noreply.github.com>
2026-03-04 21:30:23 +0100

    Create ResourceNotFoundException.java

---
+++ common/src/main/java/com/telecom/platform/common/error/ResourceNotFoundException.java
@@ -0,0 +1,11 @@
+package com.telecom.platform.common.error;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+
+    public ResourceNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
