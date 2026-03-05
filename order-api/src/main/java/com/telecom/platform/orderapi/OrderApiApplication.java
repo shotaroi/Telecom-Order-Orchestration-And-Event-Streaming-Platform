05f5dff59394f7b2d47886edc7956e4e0ad6a360
shotaroi <58347058+shotaroi@users.noreply.github.com>
2026-03-05 02:21:27 +0100

    Create OrderApiApplication.java

---
+++ order-api/src/main/java/com/telecom/platform/orderapi/OrderApiApplication.java
@@ -0,0 +1,16 @@
+package com.telecom.platform.orderapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+@ComponentScan(basePackages = {"com.telecom.platform.orderapi", "com.telecom.platform.common"})
+public class OrderApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderApiApplication.class, args);
+    }
+}
